5f84d327b68f4b8c961f885e0bbb3219cc5159f4
Atakan Demir <101272960+Atakan-Demir@users.noreply.github.com>
2024-02-13 13:04:35 +0300

    Atakan Demir

--- pdfToExcel/obj/Debug/net6.0-windows/pdfToExcel.AssemblyInfo.cs
+++ pdfToExcel/obj/Debug/net6.0-windows/pdfToExcel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pdfToExcel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a452f28eebaeb57bb723909ea26604df238c360")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a496386c3f87050588883e002cc7144e46c28359")]
 [assembly: System.Reflection.AssemblyProductAttribute("pdfToExcel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pdfToExcel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
